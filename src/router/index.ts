bf8f904af81c9948accf9366373a5fda2389b5e9
Dmitrii Gorbunov <dmitriigorbunov1998@gmail.com>
2022-06-30 17:04:02 +0300

    Build Project

---
+++ src/router/index.ts
@@ -0,0 +1,46 @@
+import { h } from "vue"
+import { createRouter, createWebHistory } from "vue-router"
+import HomePage from "@/pages/HomePage.vue"
+
+const defaultComponent = (name: string) => {
+  return {
+    render() {
+      return h("div", { className: "section" }, [name])
+    },
+  }
+}
+
+const noPageRoutes = [
+  "history",
+  "learn-more",
+  "planning",
+  "reminders",
+  "blog",
+  "calendar",
+  "todo-list",
+  "our-team",
+  "careers",
+  "about",
+  "login",
+  "register",
+]
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomePage,
+  },
+  ...noPageRoutes.map((pageName) => ({
+    path: `/${pageName}`,
+    name: pageName,
+    component: defaultComponent(pageName),
+  })),
+]
+  
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+})
+  
+export { router }
